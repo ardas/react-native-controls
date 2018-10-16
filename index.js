30b7386e92aa695f332f57e0adeb696a0da62620
Ekateryna Yakymenko <morevkaple@gmail.com>
2018-10-16 15:17:47 +0300

    add component Select, which according to prop 'multiSelect' renders appropriate component

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export { default as Grid } from "./lib/Grid";
 export { default as CodeInput } from "./lib/CodeInput";
-export { default as Select } from "./lib/Select";
+export { default as SingleSelect } from "./lib/SingleSelect";
 export { default as MultiSelect } from "./lib/MultiSelect";
+export { default as Select } from './lib/Select';
